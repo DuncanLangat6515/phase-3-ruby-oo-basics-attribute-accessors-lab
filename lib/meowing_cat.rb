4a44a3b336876abdcc29e445826846978221c6f7
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-23 10:41:55 +0300

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
 ## code your solution here. 
+require "pry"
+class Cat
+    attr_accessor :name
+    def cat_name=(value)
+        @name = value
+    end
+    def meow
+        puts "meow!"
+    end
+end
+
+binding.pry
+
